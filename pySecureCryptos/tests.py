b2d0755bdbf970083ae8472e74317a8ecc10dc3a
harshnative <harshnative@gmail.com>
2021-09-24 19:22:07 +0530

    tested encryptString and decryptString in onetimepad wrapper stringEncryptor.py

--- pySecureCryptos/tests.py
+++ pySecureCryptos/tests.py
@@ -17,7 +17,7 @@ import stringEncryptor
 
 
 
-def OnetimepadWrapperTester():
+def OnetimepadWrapperTester_password():
 
     
     def testEncryptionDecryption_password(howMany):
@@ -82,11 +82,94 @@ def OnetimepadWrapperTester():
         print(redColor + "error list = {}".format(errorList))
 
 
+
+
+
+
+
+
+
+
+
+
+def OnetimepadWrapperTester():
+
+    
+    def testEncryptionDecryption(howMany):
+
+        avgTime = 0
+        error = 0
+        errorList = []
+
+        for k in range(howMany):
+
+            print(f"\ron {k} / {howMany}" , end = "")
+
+            stringPool = []
+
+            for i in range(32 , 126):
+                stringPool.append(chr(i))
+
+            stringLength = random.randint(1 , 100000)
+
+            randomString = ""
+            for i in range(stringLength):
+                randomString = randomString + random.choice(stringPool)
+
+            randomPassword = ""
+            for i in range(random.randint(1 , 100)):
+                randomPassword = randomPassword + random.choice(stringPool)
+
+
+            start = time.time()
+            encryptedString = stringEncryptor.OnetimepadWrapper.encryptString(randomString , randomPassword)
+
+            decryptedString = stringEncryptor.OnetimepadWrapper.decryptString(encryptedString , randomPassword)
+            end = time.time()
+
+            avgTime = avgTime + (((end - start) / stringLength) * 1000)
+
+            if(decryptedString != randomString):
+                error = error + 1 
+                errorList.append(randomString , decryptedString , randomPassword , specificSHA)
+
+        avgTime = avgTime / howMany
+
+        return error , errorList , avgTime
+
+
+    print("\n\n")
+
+    # testing determinant function 
+    print(whiteColor + "Testing encryption decryption in onetimewrapper")
+
+    error , errorList , avgTime = testEncryptionDecryption(1000)
+
+    print()
+
+    if(error == 0):
+        print(blueColor + "avg time taken by testEncryptionDecryption function per encryption decyption cycle per 1000 chars = {}".format(avgTime))
+        print(greenColor + "testEncryptionDecryption function test passed")
+    else:
+        print(redColor + "testEncryptionDecryption function test failed")
+        print(redColor + "error = {} / {}".format(error , 1000))
+        print(redColor + "error list = {}".format(errorList))
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
 
     testsDict = {
-        1 : OnetimepadWrapperTester , 
-        # 2 : determinantFunctionTest , 
+        1 : OnetimepadWrapperTester_password , 
+        2 : OnetimepadWrapperTester , 
         # 3 : transposeFuncTest , 
         # 4 : meanFunctionTest , 
         # 5 : multiplyFunctionTest ,
