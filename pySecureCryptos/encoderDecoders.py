b1095697d9dff9ac550a49ac593643fdfd18ff91
harshnative <harshnative@gmail.com>
2021-11-16 19:51:38 +0530

    added yielder to encoder decoder

--- pySecureCryptos/encoderDecoders.py
+++ pySecureCryptos/encoderDecoders.py
@@ -84,6 +84,126 @@ class String2Byte:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# class containing method to encode or decode any byte 
+class Byte2String_yield:
+
+    # method to convert any byte into string
+    @classmethod
+    def encode(cls , byte):
+
+        """
+        ALGO - 
+        convert each byte into int - result = int btw 0 to 255
+
+        convert the int into string type. 
+
+        make the int string 3 chars long means if the string is 49 convert to 049
+
+        append to main string and return main string
+        """
+
+        string = ""
+
+        totalCount = len(byte)
+        currentCount = 0
+
+        for i in byte:
+            i = str(int(i))
+            i = ("0" * (3 - len(i))) + i 
+            string = string + i
+
+            currentCount = currentCount + 1
+            yield currentCount , totalCount
+
+        return string
+
+
+    # method to convert the output string from above method into byte again
+    # returns a bytearray type object
+    @classmethod
+    def decode(cls , string):
+
+        """
+        ALGO - 
+
+        traverse the string and slice the string into 3 chars long
+
+        convert the string back to int
+
+        pass the int list to bytearray and return
+        """
+
+        intList = []
+
+        totalCount = len(string) // 3 + 1
+        currentCount = 0
+
+        for i in range(0 , len(string) , 3):
+            toAppend = int(string[i : i + 3])
+            intList.append(toAppend)
+
+            currentCount = currentCount + 1
+            yield currentCount , totalCount
+
+        return bytearray(intList)
+
+
+
+class String2Byte_yield:
+
+    # method to convert string to byte
+    # returns bytearray
+    @classmethod
+    def encode(cls , string):
+
+        """
+        convert each char in string to corresponding ASCII value (int)
+
+        convert this intList to byteArray
+        """
+        intList = []
+
+        totalCount = len(string)
+        currentCount = 0
+
+        for i in string:
+            intList.append(ord(i))
+
+            currentCount = currentCount + 1
+            yield currentCount , totalCount
+
+        return bytearray(intList)
+
+
+    @classmethod
+    def decode(cls , byte):
+        string = ""
+
+        totalCount = len(byte)
+        currentCount = 0
+
+        for i in byte:
+            i = chr(int(i))
+            string = string + i
+
+            currentCount = currentCount + 1
+            yield currentCount , totalCount
+
+        return string
+
+
+
 # function to test the above class
 def __test():
     pass
