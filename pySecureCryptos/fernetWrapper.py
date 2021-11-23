5e2b3e55b92e25b6fdf24c8d12722ff25a6adb11
harshnative <harshnative@gmail.com>
2021-11-23 14:50:37 +0530

    added byte encryptor in fernet

--- pySecureCryptos/fernetWrapper.py
+++ pySecureCryptos/fernetWrapper.py
@@ -367,34 +367,125 @@ class StringEncryptor:
 
 
 
-    # method to decrypt a string
-    # returns decrypted string
-    def decrypt_string(self , enc_string):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ByteEncryptor:
+
+    def __init__(self , password , iterations=390000):
 
         # type checking the parameters
-        if(type(enc_string) != str):
-            raise ValueError("enc_string parameter expected to be of str type instead got {} type".format(type(enc_string)))
+        if(type(password) != str):
+            raise ValueError("password parameter expected to be of str type instead got {} type".format(type(password)))
 
-        chunkList = enc_string.split(":~:~:")
+        # getting md5 and sha224 hash of the password passed
+        md5_hashed_password = hashlib.md5(password.encode("utf-8")).hexdigest()
+        sha224_hashed_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
 
+        # converting sha224_hashed_password to bytes to make it usable in kdf 
+        sha224_hashed_password_bytes = bytes(sha224_hashed_password , "utf-8")
+
+        # md5_hashed_password will act as a salt
+        salt = bytes(md5_hashed_password , "utf-8")
+        
+        # deriving fernet key from the password
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=iterations,
+        )
+
+        key = base64.urlsafe_b64encode(kdf.derive(sha224_hashed_password_bytes))
+
+        # init fernet object
+        self.fernetObj = Fernet(key)
+
+        self.chunkSize = 256
+        self.chunkSize__outputSize = 440
+        self.chunkSize__inputSize = {}
+        self.__getInputSize()
+
+
+    def __getInputSize(self , max=256):
+
+        for i in range(max+1):
+            outputSize = self.__getOutputSize(i)
+            self.chunkSize__inputSize[outputSize] = i
+
+
+
+    def __getOutputSize(self , inputSize):
+        byte = b"h" * inputSize
+
+        enc_byte = self.fernetObj.encrypt(byte)
+
+        return len(enc_byte)
+
+
+
+
+
+
+
+
+    # method to encrypt a string
+    # returns a encrypted byte object
+    def encrypt_yield(self , byte):
+
+        # type checking the parameters
+        if(type(byte) != bytes):
+            raise ValueError("byte parameter expected to be of bytes type instead got {} type".format(type(byte)))
+
+        if(len(byte) == 0):
+            raise ValueError("empty byte passed")
+
+        currentYield = 1
+        len_byte = len(byte)
+
+        # calculating total yield
+        # number of chunks the data is divided into two times , one for dividing and one for ecrypting 
+        totalYields = (int(len_byte // self.chunkSize)+1)*2 
+
+        # dividing data into chunks
+        chunkList = []
+
+        for i in range(0 , len_byte , self.chunkSize):
+            if((i+self.chunkSize) < len_byte):
+                chunkList.append(byte[i : i + self.chunkSize]) 
+            else:
+                chunkList.append(byte[i : ])
+
+            yield currentYield , totalYields
+            currentYield = currentYield + 1
+        
         result = b""
 
+        # encrypt each chunk using fernet
         for i in chunkList:
-            # generator object of byte 2 string decoder
-            # need to convert back the string to byte - (byte to string was made in encryptor function)
-            encbyte_From_encString = encoderDecoders.Byte2String.decode(i)
+            encrypted_byte = self.fernetObj.encrypt(i)
 
-            decrypted_byte = self.fernetObj.decrypt(encbyte_From_encString)
-            result = result + decrypted_byte
+            yield currentYield , totalYields
+            currentYield = currentYield + 1
 
+            result = result + encrypted_byte + b":~:~:"
 
-        # convert the byte object returned from fernet back to string
-        stringFromByte  = encoderDecoders.String2Byte.decode(result)
-
-
-        return stringFromByte
-
-    
+        return result[:-5]
 
 
 
@@ -406,18 +497,21 @@ class StringEncryptor:
 
 
 
-
-    # method to decrypt a string
-    # returns decrypted string
-    # else returns a byte object
-    def decrypt_byte(self , enc_byte):
+    # method to decrypt a byte
+    # returns a bytes object
+    def decrypt_yield(self , enc_byte):
 
         # type checking the parameters
         if(type(enc_byte) != bytes):
             raise ValueError("enc_byte parameter expected to be of bytes type instead got {} type".format(type(enc_byte)))
 
+        currentYield = 1
+
         chunkList = enc_byte.split(b":~:~:")
 
+        # calculating total yield
+        # number of chunks the data is divided into 
+        totalYields = len(chunkList)
 
         result = b""
 
@@ -425,11 +519,88 @@ class StringEncryptor:
             decrypted_byte = self.fernetObj.decrypt(i)
             result = result + decrypted_byte
 
-        # convert the byte object returned from fernet back to string
-        stringFromByte  = encoderDecoders.String2Byte.decode(result)
+            yield currentYield , totalYields
+            currentYield = currentYield + 1
+
+        return result
 
 
-        return stringFromByte
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # method to encrypt a string
+    # returns a encrypted byte object
+    def encrypt(self , byte):
+
+        # type checking the parameters
+        if(type(byte) != bytes):
+            raise ValueError("byte parameter expected to be of bytes type instead got {} type".format(type(byte)))
+
+        if(len(byte) == 0):
+            raise ValueError("empty byte passed")
+
+        currentYield = 1
+        len_byte = len(byte)
+
+        # dividing data into chunks
+        chunkList = []
+
+        for i in range(0 , len_byte , self.chunkSize):
+            if((i+self.chunkSize) < len_byte):
+                chunkList.append(byte[i : i + self.chunkSize]) 
+            else:
+                chunkList.append(byte[i : ])
+
+        result = b""
+
+        # encrypt each chunk using fernet
+        for i in chunkList:
+            encrypted_byte = self.fernetObj.encrypt(i)
+
+            result = result + encrypted_byte + b":~:~:"
+
+        return result[:-5]
+
+
+
+
+
+
+
+
+
+
+
+    # method to decrypt a byte
+    # returns a bytes object
+    def decrypt(self , enc_byte):
+
+        # type checking the parameters
+        if(type(enc_byte) != bytes):
+            raise ValueError("enc_byte parameter expected to be of bytes type instead got {} type".format(type(enc_byte)))
+
+        currentYield = 1
+
+        chunkList = enc_byte.split(b":~:~:")
+
+        result = b""
+
+        for i in chunkList:
+            decrypted_byte = self.fernetObj.decrypt(i)
+            result = result + decrypted_byte
+
+        return result
     
 
 
@@ -611,5 +782,84 @@ def __test3():
 
 
 
+
+
+
+def __test4():
+
+    obj = ByteEncryptor("hello world")
+
+    myByte = b"my name is john" * 11233
+
+    print("\nlen myByte = {}\n".format(len(myByte)))
+
+    objGen_encryptor = obj.encrypt_yield(myByte)
+
+    print()
+    while(True):
+        try:
+            currentYield , totalYields = next(objGen_encryptor)
+            print("\r{} , {}".format(currentYield , totalYields) , end="")
+
+        except StopIteration as ex:
+            encryptedByte = ex.value
+            break
+    print()
+
+    print("\nlen encrypted byte = {}\n".format(len(encryptedByte)))
+
+    objGen_decryptor = obj.decrypt_yield(encryptedByte)
+
+    print()
+    while(True):
+        try:
+            currentYield , totalYields = next(objGen_decryptor)
+            print("\r{} , {}".format(currentYield , totalYields) , end="")
+
+        except StopIteration as ex:
+            decryptedByte = ex.value
+            break
+    print()
+
+    print("\nlen decryptedByte = {}\n".format(len(decryptedByte)))
+
+    if(decryptedByte == myByte):
+        print("\nok")
+    else:
+        print("\nerror")
+
+
+
+
+
+
+
+
+
+
+
+def __test5():
+
+    obj = ByteEncryptor("hello world")
+
+    myByte = b"my name is john"
+
+    print("\nlen myByte = {}\n".format(len(myByte)))
+
+    encryptedByte = obj.encrypt(myByte)
+
+    print("\nencryptedByte = {}\n".format(encryptedByte))
+
+    decryptedByte = obj.decrypt(encryptedByte)
+
+    print("\ndecryptedByte = {}\n".format(decryptedByte))
+
+    if(decryptedByte == myByte):
+        print("\nok")
+    else:
+        print("\nerror")
+
+
+
 if __name__ == "__main__":
-    __test1()
+    __test5()
