a7e55a99dce26d3144e8c677390e8ac64b51456c
harshnative <harshnative@gmail.com>
2021-11-29 15:14:25 +0530

    added shuffler test

---
+++ tests/shuffler_test/shufferTest_byte.py
@@ -0,0 +1,94 @@
+import string
+import random
+import time
+import secrets
+
+
+# create objects to print colored strings
+from colored import fg
+blueColor = fg('blue')
+greenColor = fg('green')
+redColor = fg('red')
+yellowColor = fg('yellow')
+whiteColor = fg('white')
+
+# file name in which the errors will be written to 
+fileName = "testResult"
+
+# list of lettes , nums , special chars
+mainList = list(string.ascii_letters + string.digits + "!@#$%^&*(){}[]:;'<,>./")
+
+
+# importing module for testing
+from pySecureCryptos.shuffler import Shuffler
+
+
+
+# main function to run the test
+def shufflerTester_Byte(howManyTimes , writeErrors = True):
+    avgTime = 0
+    errorList = []
+    totalErrors = 0
+
+    # repeat the test howManyTimes
+    for k in range(howManyTimes):
+
+        print(f"\ron {k} / {howManyTimes}" , end = "")
+
+        # byte size
+        byteLength = random.randint(1 , 1000)
+
+        # byte for shuffler test
+        randomByte = secrets.token_bytes(byteLength)
+
+
+        # string for shuffle test
+        randList = random.choices(mainList , k=byteLength)
+        randString2 = "".join(randList)
+
+        # executing the functions to test and calculating time
+        startTime = time.perf_counter()
+        shuffledByte = Shuffler.shuffle_byte(randomByte , randString2)
+
+        deShuffledByte = Shuffler.unShuffle_byte(shuffledByte , randString2)
+        endTime = time.perf_counter()
+
+        # avgTime
+        avgTime = avgTime + (endTime - startTime)
+
+        # if the result is not true than add to error list
+        if(deShuffledByte != randomByte):
+            errorList.append([randString2 , shuffledByte , deShuffledByte])
+            totalErrors = totalErrors + 1
+
+    # write the error list to the file
+    if(totalErrors != 0):
+        with open(fileName + "shufflerTester_string" + ".txt" , "w") as file:
+            for i in errorList:
+                for j in i:
+                    file.write(str(j))
+                    file.write("\n")
+                file.write("\n\n")
+
+    avgTime = avgTime / howManyTimes
+
+    print()
+
+    # print the result
+    if(totalErrors == 0):
+        print(blueColor + "avg time taken by function per cycle = {}".format(avgTime))
+        print(greenColor + "function test passed")
+    else:
+        print(redColor + "function test failed")
+        print(blueColor + "errors has been logged to the (file + functionName).txt")
+
+# execute the function
+shufflerTester_Byte(1000)
+
+            
+
+
+            
+
+
+
